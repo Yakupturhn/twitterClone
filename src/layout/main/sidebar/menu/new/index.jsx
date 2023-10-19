703adb8e3ff892f11251ffabecd3b723d50fbc04
Yakupturhn <yakupturhan27@gmail.com>
2023-10-19 17:36:56 +0300

    continue

---
+++ src/layout/main/sidebar/menu/new/index.jsx
@@ -0,0 +1,8 @@
+import Button from "~/components/button";
+export default function New() {
+  return (
+    <div className="py-4 w-[90%]">
+      <Button>Gönder</Button>
+    </div>
+  );
+}
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,19 +1,33 @@
 import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "~/layout/main";
+import ErrorPage from "~/pages/errorpage";
+import Explore from "~/pages/explore";
+import Home from "~/pages/home";
+import Notifications from "~/pages/notifications";
 
 const routes = createBrowserRouter([
   {
     path: "/",
-    element: "anasayfa componenti",
-  },
-  {
-    path: "/explore",
-    element: "explore componenti",
-  },
-  {
-    path: "/notifications",
-    element: "notifications componenti",
+    element: <MainLayout />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "explore",
+        element: <Explore />,
+      },
+      {
+        path: "notifications",
+        element: <Notifications />,
+      },
+      {
+        path: "*",
+        element: <ErrorPage />,
+      },
+    ],
   },
 ]);
 
-// defauult olarak export ettiysem import ederken istedigim isimle alabiliyorum
 export default routes;
